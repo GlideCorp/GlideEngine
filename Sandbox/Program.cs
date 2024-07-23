f2f3163e1b87b906f3c4afb5261331d299fa93ee
Leonardo Sinceri <43849125+TQCnoARosto@users.noreply.github.com>
2024-07-23 19:35:18 +0200

    Raw loger implementation

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,36 +1,17 @@
-﻿using Silk.NET.Input;
-using Silk.NET.Maths;
-using Silk.NET.Windowing;
-using Silk.NET.Windowing.Glfw;
-using Silk.NET.OpenGL;
+﻿
+using Core.Logs;
+
 public class Program
 {
-    private static IWindow? _window;
-    private static GL? _gl;
-
     public static void Main(string[] args)
     {
-        WindowOptions options = WindowOptions.Default with
-        {
-            Size = new Vector2D<int>(800, 600),
-            Title = "GlideEngine",
-            API = new(ContextAPI.OpenGL, ContextProfile.Core, ContextFlags.Debug, new(4, 6))
-        };
+        Task.Delay(2000).Wait();
+        Logger.Info("Ciaoo");
 
-        _window = Window.Create(options);
-        _window.Load += OnLoad;
-        _window.Update += OnUpdate;
-        _window.Render += OnRender;
-        _window.Run();
+        Task.Delay(2000).Wait();
+        Logger.Warning("Ciaoo");
 
+        Task.Delay(2000).Wait();
+        Logger.Error("Ciaoo");
     }
-    private static void OnLoad()
-    {
-        _gl = _window.CreateOpenGL();
-        Console.WriteLine($"{_gl?.GetStringS(GLEnum.Vendor)}\n{_gl?.GetStringS(GLEnum.Version)}\n");
-    }
-
-    private static void OnUpdate(double deltaTime) { }
-
-    private static void OnRender(double deltaTime) { }
 }
