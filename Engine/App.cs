6e96308662c058def6cb740658d046351340fe37
Bolla <dado.delgaudio@gmail.com>
2024-07-23 17:34:26 +0200

    Simple App class for east GL and Window access

---
+++ Engine/App.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.SDL;
+using Silk.NET.Windowing.Glfw;
+
+namespace Engine
+{
+    public static class App
+    {
+
+        public static GL Gl { get; private set; }
+
+        public static IWindow Window { get; private set; }
+
+        
+        public static void Initialize()
+        {
+            WindowOptions options = WindowOptions.Default with
+            {
+                Size = new Vector2D<int>(800, 600),
+                Title = "GlideEngine",
+                API = new(ContextAPI.OpenGL, ContextProfile.Core, ContextFlags.Debug, new(4, 6))
+            };
+
+            //TODO: Rivedere nomeclatura per le proprietà
+            Window = Silk.NET.Windowing.Window.Create(options);
+            Window.Load += OnLoad;
+            Window.Update += OnUpdate;
+            Window.Render += OnRender;
+            Window.Run();
+        }
+
+        public static void Run()
+        {
+            Window.Run();
+        }
+
+        public static void End()
+        {
+            Window.Dispose();
+        }
+
+        private static void OnLoad()
+        {
+            Gl = Window.CreateOpenGL();
+
+            //TODO: Rimuove una volta implementato sistema di logs
+            Console.WriteLine($"{Gl?.GetStringS(GLEnum.Vendor)}\n{Gl?.GetStringS(GLEnum.Version)}\n");
+        }
+
+        private static void OnUpdate(double deltaTime) { }
+
+        private static void OnRender(double deltaTime) { }
+    }
+}
--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,36 +1,7 @@
-﻿using Silk.NET.Input;
-using Silk.NET.Maths;
-using Silk.NET.Windowing;
-using Silk.NET.Windowing.Glfw;
-using Silk.NET.OpenGL;
-public class Program
-{
-    private static IWindow? _window;
-    private static GL? _gl;
+﻿using Engine;
 
-    public static void Main(string[] args)
-    {
-        WindowOptions options = WindowOptions.Default with
-        {
-            Size = new Vector2D<int>(800, 600),
-            Title = "GlideEngine",
-            API = new(ContextAPI.OpenGL, ContextProfile.Core, ContextFlags.Debug, new(4, 6))
-        };
+App.Initialize();
 
-        _window = Window.Create(options);
-        _window.Load += OnLoad;
-        _window.Update += OnUpdate;
-        _window.Render += OnRender;
-        _window.Run();
+App.Run();
 
-    }
-    private static void OnLoad()
-    {
-        _gl = _window.CreateOpenGL();
-        Console.WriteLine($"{_gl?.GetStringS(GLEnum.Vendor)}\n{_gl?.GetStringS(GLEnum.Version)}\n");
-    }
-
-    private static void OnUpdate(double deltaTime) { }
-
-    private static void OnRender(double deltaTime) { }
-}
+App.End();
