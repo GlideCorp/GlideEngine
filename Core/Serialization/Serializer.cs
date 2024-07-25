50d464df40087d6426c2c26a00d84361a23ff37c
Leonardo Sinceri <43849125+TQCnoARosto@users.noreply.github.com>
2024-07-25 17:23:30 +0200

    Serializing from file

---
+++ Core/Serialization/Serializer.cs
@@ -0,0 +1,51 @@
+﻿using Core.Logs;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Core.Serialization
+{
+    public class Serializer
+    {
+        private static Serializer? _instance = null;
+        private static Serializer Instance
+        {
+            get
+            {
+                _instance ??= new();
+                return _instance;
+            }
+        }
+
+        private JsonSerializerOptions SerializationOptions { get; init; }
+
+        private Serializer()
+        {
+            SerializationOptions = new()
+            {
+                WriteIndented = true,
+            };
+        }
+
+        public static string Serialize<T>(T @object)
+        {
+            return JsonSerializer.Serialize(@object, Instance.SerializationOptions);
+        }
+
+        public static T? Deserialize<T>(string text)
+        {
+            return JsonSerializer.Deserialize<T>(text, Instance.SerializationOptions);
+        }
+
+        public static T? Deserialize<T>(FileInfo file)
+        {
+            if (!file.Exists)
+            {
+                Logger.Error("File not found");
+                return default;
+            }
+
+            string jsonString = File.ReadAllText(file.FullName);
+            return Deserialize<T>(jsonString);
+        }
+    }
+}
