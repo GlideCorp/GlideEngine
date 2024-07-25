219a493183de4d78822b8a800aaa9d4e21e3142f
Bolla <dado.delgaudio@gmail.com>
2024-07-26 00:32:21 +0200

    Changed Gui default font

--- Editor/EditorApp.cs
+++ Editor/EditorApp.cs
@@ -28,11 +28,16 @@ namespace Editor
             base.OnLoad();
 
             InputContext = Window.CreateInput();
-            ImGuiController = new ImGuiController(Context, Window, InputContext);
+
+            ImGuiFontConfig fontConfig = new("resources\\fonts\\Arvo-Regular.ttf", 15);
+            ImGuiController = new ImGuiController(Context,Window, InputContext, fontConfig);
 
             var io = ImGui.GetIO();
             io.BackendFlags = ImGuiBackendFlags.None;
             io.ConfigFlags |= ImGuiConfigFlags.DockingEnable;
+
+            //io.Fonts.AddFontFromFileTTF("resources\\fonts\\Arvo-Regular.ttf", 15.0f, null, io.Fonts.GetGlyphRangesDefault());
+            //RebuildFontAtlas();
 
             FileStream stream = File.OpenRead("resources\\test.png");
             testTexture = Texture2D.FromStream(stream);
@@ -68,6 +73,28 @@ namespace Editor
             ImGui.End();
 
             ImGuiController?.Render();
+        }
+
+        //Sposta in una classe a parte, idealmente una che gestisce in modo più specifico DearImgui nel contesto di un Editor
+        //Forse the cherno aveva un video al riguardo boh :)
+        //Senno una cosa simile: https://github.com/dovker/Monogame.ImGui/blob/master/MonoGame.ImGui/ImGUIRenderer.cs#L29
+        unsafe void RebuildFontAtlas()
+        {     
+            // Get font texture from ImGui
+            var io = ImGui.GetIO();
+            io.Fonts.GetTexDataAsRGBA32(out byte* pixelData, out var width, out var height, out var bytesPerPixel);
+
+            // Copy the data to a managed array
+            var pixels = new byte[width * height * bytesPerPixel];
+            Marshal.Copy(new IntPtr(pixelData), pixels, 0, pixels.Length);
+
+            // Create and register the texture
+            Texture2D texture = new(width, height);
+            texture.SetData(pixels);
+
+            // Let ImGui know where to find the texture
+            io.Fonts.SetTexID((nint)texture.TextureID);
+            io.Fonts.ClearTexData(); // Clears CPU side texture data
         }
 
     }
