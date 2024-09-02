b8a834515752e836cc9010b741a8cd47cdd14c73
Bolla <dado.delgaudio@gmail.com>
2024-09-02 22:28:44 +0200

    matrix4x4 implementation

--- Core/Maths/Matrices/Matrix4x4.cs
+++ Core/Maths/Matrices/Matrix4x4.cs
@@ -3,14 +3,107 @@ using Core.Logs;
 using Core.Maths.Vectors;
 using System.Numerics;
 using System.Runtime.Intrinsics;
+using Vector3 = Core.Maths.Vectors.Vector3;
+using Vector4 = Core.Maths.Vectors.Vector4;
 
 namespace Core.Maths.Matrices
 {
     public class Matrix4x4() : RootMatrix4x4<float>()
     {
-        public Matrix4x4(Vector4 column0, Vector4 column1, Vector4 column2, Vector4 column3)
-        {
+        public new static Matrix4x4 Identity => new(
+            new(1, 0, 0, 0),
+            new(0, 1, 0, 0),
+            new(0, 0, 1, 0),
+            new(0, 0, 0, 1)
+        );
 
+        public Matrix4x4(Vector4 column0, Vector4 column1, Vector4 column2, Vector4 column3) : this()
+        {
+            Values[0, 0] = column0.X;
+            Values[0, 1] = column1.X;
+            Values[0, 2] = column2.X;
+            Values[0, 3] = column3.X;
+
+            Values[1, 0] = column0.Y;
+            Values[1, 1] = column1.Y;
+            Values[1, 2] = column2.Y;
+            Values[1, 3] = column3.Y;
+
+            Values[2, 0] = column0.Z;
+            Values[2, 1] = column1.Z;
+            Values[2, 2] = column2.Z;
+            Values[2, 3] = column3.Z;
+
+            Values[3, 0] = column0.W;
+            Values[3, 1] = column1.W;
+            Values[3, 2] = column2.W;
+            Values[3, 3] = column3.W;
+        }
+
+        public static Matrix4x4 Translate(Vector3 vector)
+        {
+            Matrix4x4 result = Identity;
+            result.Values[0, 3] = vector.X;
+            result.Values[1, 3] = vector.Y;
+            result.Values[2, 3] = vector.Z;
+            result.Values[3, 3] = 1;
+            return result;
+        }
+
+        public static Matrix4x4 Rotate(Quaternion quaternion)
+        {
+            Matrix4x4 result = Identity;
+
+            // Precalculate coordinate products
+            float x = quaternion.X * 2.0F;
+            float y = quaternion.Y * 2.0F;
+            float z = quaternion.Z * 2.0F;
+            float xx = quaternion.X * x;
+            float yy = quaternion.Y * y;
+            float zz = quaternion.Z * z;
+            float xy = quaternion.X * y;
+            float xz = quaternion.X * z;
+            float yz = quaternion.Y * z;
+            float wx = quaternion.W * x;
+            float wy = quaternion.W * y;
+            float wz = quaternion.W * z;
+
+            result.Values[0, 0] = 1.0f - (yy + zz);
+            result.Values[1, 0] = (xy + wz);
+            result.Values[2, 0] = (xz + wy);
+            result.Values[3, 0] = 0.0f;
+
+            result.Values[0, 1] = (xy - wz);
+            result.Values[1, 1] = 1.0f - (xx + zz);
+            result.Values[2, 1] = (yz + wx);
+            result.Values[3, 1] = 0.0f;
+
+            result.Values[0, 2] = (xz + wy);
+            result.Values[1, 2] = (yz - wx);
+            result.Values[2, 2] = 1.0f - (xx + yy);
+            result.Values[3, 2] = 0.0F;
+
+            result.Values[0, 3] = 0.0f;
+            result.Values[1, 3] = 0.0f;
+            result.Values[2, 3] = 0.0f;
+            result.Values[3, 3] = 1.0f;
+
+            return result;
+        }
+
+        public static Matrix4x4 Scale(Vector3 vector)
+        {
+            Matrix4x4 result = new()
+            {
+                Values =
+                {
+                    [0, 0] = vector.X,
+                    [1, 1] = vector.Y,
+                    [2, 2] = vector.Z,
+                    [3, 3] = 1
+                }
+            };
+            return result;
         }
     }
 
@@ -227,72 +320,6 @@ namespace Core.Maths.Matrices
         public static bool operator >=(RootMatrix4x4<T> left, RootMatrix4x4<T> right) { throw new NotImplementedException(); }
         public static bool operator <(RootMatrix4x4<T> left, RootMatrix4x4<T> right) { throw new NotImplementedException(); }
         public static bool operator <=(RootMatrix4x4<T> left, RootMatrix4x4<T> right) { throw new NotImplementedException(); }
-
-        public static RootMatrix4x4<T> Translate(Vector3<T> vector)
-        {
-            RootMatrix4x4<T> result = Identity;
-            result.Values[0, 3] = vector.X;
-            result.Values[1, 3] = vector.Y;
-            result.Values[2, 3] = vector.Z;
-            result.Values[3, 3] = T.One;
-            return result;
-        }
-
-        public static RootMatrix4x4<float> Rotate(Quaternion quaternion)
-        {
-            RootMatrix4x4<float> result = RootMatrix4x4<float>.Identity;
-            
-            // Precalculate coordinate products
-            float x = quaternion.X * 2.0F;
-            float y = quaternion.Y * 2.0F;
-            float z = quaternion.Z * 2.0F;
-            float xx = quaternion.X * x;
-            float yy = quaternion.Y * y;
-            float zz = quaternion.Z * z;
-            float xy = quaternion.X * y;
-            float xz = quaternion.X * z;
-            float yz = quaternion.Y * z;
-            float wx = quaternion.W * x;
-            float wy = quaternion.W * y;
-            float wz = quaternion.W * z;
-
-            result.Values[0, 0] = 1.0f - (yy + zz);
-            result.Values[1, 0] = (xy + wz);
-            result.Values[2, 0] = (xz + wy);
-            result.Values[3, 0] = 0.0f;
-
-            result.Values[0, 1] = (xy - wz);
-            result.Values[1, 1] = 1.0f - (xx + zz);
-            result.Values[2, 1] = (yz + wx);
-            result.Values[3, 1] = 0.0f;
-
-            result.Values[0, 2] = (xz + wy);
-            result.Values[1, 2] = (yz - wx);
-            result.Values[2, 2] = 1.0f - (xx + yy);
-            result.Values[3, 2] = 0.0F;
-
-            result.Values[0, 3] = 0.0f;
-            result.Values[1, 3] = 0.0f;
-            result.Values[2, 3] = 0.0f;
-            result.Values[3, 3] = 1.0f;
-            
-            return result;
-        }
-
-        public static RootMatrix4x4<T> Scale(Vector3<T> vector)
-        {
-            RootMatrix4x4<T> result = new()
-            {
-                Values =
-                {
-                    [0, 0] = vector.X,
-                    [1, 1] = vector.Y,
-                    [2, 2] = vector.Z,
-                    [3, 3] = T.One
-                }
-            };
-            return result;
-        }
 
         public RootMatrix4x4<T> Transpose()
         {
