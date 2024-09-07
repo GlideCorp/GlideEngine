28ba5832e341891d971974207c90ecdc4818f946
Leonardo Sinceri <43849125+TQCnoARosto@users.noreply.github.com>
2024-09-07 23:58:43 +0200

    Delete Sandbox directory

--- Sandbox/Program.cs
+++
@@ -1,8 +0,0 @@
-
-using Core.Locations;
-
-string alphabeticalLocation = "game:entities:player:components:transform";
-Location location1 = new(alphabeticalLocation);
-Location location2 = new(alphabeticalLocation);
-
-bool equals = location1 == location2;
