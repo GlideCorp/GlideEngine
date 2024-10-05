f1593b117f5c988de62432605cdc0f38b1106ede
Leonardo Sinceri <43849125+TigrottoL9@users.noreply.github.com>
2024-10-05 22:55:18 +0200

    Create Stack.cs

--- Core/Collections/Interfaces/IStack.cs
+++ Core/Collections/Interfaces/IStack.cs
@@ -1,7 +1,12 @@
 ﻿
 namespace Core.Collections.Interfaces
 {
-    public interface IStack<in TValue> : ICollection<TValue>
+    public interface IStack<TValue> : ICollection<TValue>
     {
+        public void Push(TValue value);
+
+        public TValue Pop();
+
+        public TValue Peek();
     }
 }
---
+++ Core/Collections/Stacks/Stack.cs
@@ -0,0 +1,27 @@
+﻿
+using Core.Collections.Interfaces;
+using Core.Collections.LinkedLists;
+
+namespace Core.Collections.Stacks
+{
+    public class Stack<TValue> : IStack<TValue>
+    {
+        public int Count => BackingCollection.Count;
+        private SinglyLinkedList<TValue> BackingCollection { get; set; } = new();
+
+        public void Insert(TValue value) { Push(value); }
+
+        public void Push(TValue value) { BackingCollection.InsertFirst(value); }
+
+        public TValue Pop()
+        {
+            TValue value = BackingCollection.ValueAt(0);
+            BackingCollection.RemoveFirst();
+            return value;
+        }
+
+        public TValue Peek() { return BackingCollection.ValueAt(0); }
+
+        public void Clear() { BackingCollection.Clear(); }
+    }
+}
