bcc83e715357c0b8149c0370c7d17b783d71f975
Leonardo Sinceri <43849125+TigrottoL9@users.noreply.github.com>
2024-10-05 22:50:49 +0200

    Create Queue.cs

---
+++ Core/Collections/Queues/Queue.cs
@@ -0,0 +1,27 @@
+﻿
+using Core.Collections.Interfaces;
+using Core.Collections.LinkedLists;
+
+namespace Core.Collections.Queues
+{
+    public class Queue<TValue> : IQueue<TValue>
+    {
+        public int Count => BackingCollection.Count;
+        private SinglyLinkedList<TValue> BackingCollection { get; set; } = new();
+
+        public void Insert(TValue value) { Enqueue(value); }
+
+        public void Enqueue(TValue value) { BackingCollection.InsertLast(value); }
+
+        public TValue Dequeue()
+        {
+            TValue value = BackingCollection.ValueAt(0);
+            BackingCollection.RemoveFirst();
+            return value;
+        }
+
+        public TValue Peek() { return BackingCollection.ValueAt(0); }
+
+        public void Clear() { BackingCollection.Clear(); }
+    }
+}
